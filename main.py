78ec2339ccb8fb375365d47d2154fa0cb920a48c
Pranay Musalimadugu <pmusalimadugu@gmail.com>
2021-12-02 18:23:12 -0500

    fix dependencies, add basic tensor test

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 
+import tensorflow as tf
+
+
 def main():
-    print("Hello, world!")
+    # tf.ones() creates a shape [1,2,3] tensor full of ones
+    tensor1 = tf.ones([1, 2, 3])
+    # reshape existing data to shape [2,3,1]
+    tensor2 = tf.reshape(tensor1, [2, 3, 1])
+    # -1 tells the tensor to calculate the size of the dimension in that place
+    tensor3 = tf.reshape(tensor2, [3, -1])
+    # this will reshape the tensor to [3,3]
+    print(tensor1)
+    # The numer of elements in the reshaped tensor MUST match the number in the original
 
 
 if __name__ == "__main__":
